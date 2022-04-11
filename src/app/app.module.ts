15b47a697c87ad73fb073b967bf5b91227732fb9
r00t254 <root254.mary@gmail.com>
2022-04-11 23:36:15 +0300

    created strickthrough directive files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
 import { QuotesDetailComponent } from './quotes-detail/quotes-detail.component';
+import { StrickthroughDirective } from './strickthrough.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuotesComponent,
-    QuotesDetailComponent
+    QuotesDetailComponent,
+    StrickthroughDirective
   ],
   imports: [
     BrowserModule,
---
+++ src/app/strickthrough.directive.spec.ts
@@ -0,0 +1,8 @@
+import { StrickthroughDirective } from './strickthrough.directive';
+
+describe('StrickthroughDirective', () => {
+  it('should create an instance', () => {
+    const directive = new StrickthroughDirective();
+    expect(directive).toBeTruthy();
+  });
+});
