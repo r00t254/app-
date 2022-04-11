3527687b24ec1c37a907b97f94771474d8eea1f2
r00t254 <root254.mary@gmail.com>
2022-04-11 23:22:50 +0300

    created quotes-detail file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
+import { QuotesDetailComponent } from './quotes-detail/quotes-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuotesComponent
+    QuotesComponent,
+    QuotesDetailComponent
   ],
   imports: [
     BrowserModule,
