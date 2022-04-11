f0046e80660ccb6ac27216887f73aebd59c763f8
r00t254 <root254.mary@gmail.com>
2022-04-12 00:10:08 +0300

    add changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,35 @@
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+
+// import { AppRoutingModule } from './app-routing.module';
+// import { AppComponent } from './app.component';
+// import { QuotesComponent } from './quotes/quotes.component';
+// import { QuotesDetailComponent } from './quotes-detail/quotes-detail.component';
+// import { StrickthroughDirective } from './strickthrough.directive';
+// import { DateCountPipe } from './date-count.pipe';
+// import { QuoteFormComponent } from './quote-form/quote-form.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { QuotesComponent } from './quotes/quotes.component';
-import { QuotesDetailComponent } from './quotes-detail/quotes-detail.component';
-import { StrickthroughDirective } from './strickthrough.directive';
-import { DateCountPipe } from './date-count.pipe';
+import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
+import { QuoteComponent } from './quotes/quotes.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { FormsModule } from '@angular/forms';
+import { DateCountPipe } from './date-count.pipe';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     QuotesComponent,
     QuotesDetailComponent,
-    StrickthroughDirective,
     DateCountPipe,
     QuoteFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,4 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+// import { Component, OnInit } from '@angular/core';
+
+// @Component({
+//   selector: 'app-quote-form',
+//   templateUrl: './quote-form.component.html',
+//   styleUrls: ['./quote-form.component.css']
+// })
+// export class QuoteFormComponent implements OnInit {
+
+//   constructor() { }
+
+//   ngOnInit(): void {
+//   }
+
+// }
+import { Component, OnInit,EventEmitter, Output } from '@angular/core';
+import { Quotes } from '../quotes';
 
 @Component({
   selector: 'app-quote-form',
@@ -6,6 +22,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote-form.component.css']
 })
 export class QuoteFormComponent implements OnInit {
+  newQuote = new Quotes(0, "", "", "", "", new Date(), 0, 0);
+
+  @Output () addQuote = new EventEmitter<Quotes>();
+
+  submitQuote (){
+    this.addQuote.emit(this.newQuote);
+    this.newQuote = new Quotes(0,"", "", "","", new Date(), 0, 0);
+  }
 
   constructor() { }
 
@@ -13,3 +37,4 @@ export class QuoteFormComponent implements OnInit {
   }
 
 }
+
